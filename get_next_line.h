910380e5e6a329e6290ebb62dd3245e4dd6a1151
Alexandr SARANDI <asarandi@e1z2r9p5.42.us.org>
2017-10-14 12:54:31 -0700

    first commit

---
+++ get_next_line.h
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asarandi <asarandi@student.42.us.org>      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/10/14 12:11:19 by asarandi          #+#    #+#             */
+/*   Updated: 2017/10/14 12:51:25 by asarandi         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# include <stdlib.h>
+# include <unistd.h>
+
+# define BUFF_SIZE 32
+
+typedef unsigned char	t_uc;
+
+int	get_next_line(const int fd, char **line);
+
+#endif
